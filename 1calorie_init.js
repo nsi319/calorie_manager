96e593c0c88d7ab29ee70af19cbecf7f8bfce56e
nsi319 <sairam.nareniyer@gmail.com>
2019-05-26 15:31:27 +0530

    second commit

--- 1calorie_init.js
+++
@@ -1,7 +0,0 @@
-localStorage.clear();                // to clear exixting ls
-function save_data() {
-Storage.setItem("Sai_inidata","no");        // create intial values
-localStorage.setItem("Pai_inidata","no");
-localStorage.setItem("entryvalue",1);        // entry value
-document.getElementById("result").innerHTML="INITIALIZATION SUCCESSFULL";
-}
